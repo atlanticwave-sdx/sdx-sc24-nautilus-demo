7d01ad09611af924bda80982e54bb16363e60824
Italo Valcy <italo@ampath.net>
2025-03-10 10:08:44 -0300

    feat: add support for configure static IP address

--- nautilus_utils.py
+++ nautilus_utils.py
@@ -34,7 +34,7 @@ def delete_vlan(vlan):
     # TODO: wait to remove?
     return True
 
-def create_vlan(iface, vlan):
+def create_vlan(iface, vlan, ipv4_addr=None, ipv6_addr=None):
     try:
         resp = k8s_custom.get_namespaced_custom_object(group="k8s.cni.cncf.io", version="v1", namespace=NAMESPACE, plural="network-attachment-definitions", name=f"multus{vlan}")
     except client.exceptions.ApiException:
@@ -42,6 +42,23 @@ def create_vlan(iface, vlan):
     else:
         if not delete_vlan(vlan):
             return False
+
+    ip_addr_mgmt = {}
+    if ipv4_addr is None and ipv6_addr is None:
+        ip_addr_mgmt = {
+            "type": "host-local",
+            "subnet": "10.1.11.0/24",
+            "rangeStart": "10.1.11.10",
+            "rangeEnd": "10.1.11.200"
+        }
+    if ipv4_addr:
+        ip_addr_mgmt["type"] = "static"
+        ip_addr_mgmt.setdefault("addresses", [])
+        ip_addr_mgmt["addresses"].append({"address": ipv4_addr})
+    if ipv6_addr:
+        ip_addr_mgmt["type"] = "static"
+        ip_addr_mgmt.setdefault("addresses", [])
+        ip_addr_mgmt["addresses"].append({"address": ipv6_addr})
 
     net_dict = {
         'apiVersion': 'k8s.cni.cncf.io/v1',
@@ -59,12 +76,7 @@ def create_vlan(iface, vlan):
                     "type": "vlan",
                     "master": iface,
                     "vlanId": vlan,
-                    "ipam": {
-                        "type": "host-local",
-                        "subnet": "10.1.11.0/24",
-                        "rangeStart": "10.1.11.10",
-                        "rangeEnd": "10.1.11.200"
-                    }
+                    "ipam": ip_addr_mgmt,
                 }]
             }),
         },
@@ -88,6 +100,7 @@ def delete_deployment(name):
     else:
         print("Timeout waiting for deployment to be deleted")
         return False
+    KNOWN_PODS.pop(name, None)
     return True
 
 def create_deployment(
@@ -95,7 +108,9 @@ def create_deployment(
     image="gitlab-registry.nrp-nautilus.io/prp/perfsonar/testpoint",
     nrp_node="k8s-gen4-01.ampath.net",
     node_iface="enp193s0f1",
-    vlan=3888
+    vlan=3888,
+    ipv4_addr=None,
+    ipv6_addr=None,
 ):
     try:
         resp = app_v1.read_namespaced_deployment(namespace=NAMESPACE, name=name)
@@ -106,7 +121,7 @@ def create_deployment(
             return False
 
     if node_iface and vlan:
-        create_vlan(node_iface, vlan)
+        create_vlan(node_iface, vlan, ipv4_addr, ipv6_addr)
 
     deploy_dict = {
         'apiVersion': 'apps/v1',
